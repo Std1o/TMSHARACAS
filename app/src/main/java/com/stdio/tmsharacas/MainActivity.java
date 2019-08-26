206ce2945db520730f2e074a129fefc2c2b5dfa0
Stdio <kwork-stdio@mail.ru>
2019-08-27 01:02:12 +0300

    0.0.2

--- app/src/main/java/com/stdio/tmsharacas/MainActivity.java
+++ app/src/main/java/com/stdio/tmsharacas/MainActivity.java
@@ -1,12 +1,21 @@
 package com.stdio.tmsharacas;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Environment;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
@@ -20,14 +29,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import static com.stdio.tmsharacas.WebViewHelper.afterChosePic;
+import static com.stdio.tmsharacas.WebViewHelper.cameraUri;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String contextPath;
+    private String originalCookiePath;
+    private String myCookieDbPath;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static final String TAG = "MainActivity";
+    public static final int REQUEST_CAMERA = 1;
+    public static final int REQUEST_CHOOSE = 2;
+    private WebView mWebView;
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String extDir = Environment.getExternalStorageDirectory().getPath();
+
+        myCookieDbPath = extDir + "/MyCookies";
+        contextPath = "/data/data/" + getApplicationContext().getPackageName() + "/";
+        originalCookiePath = contextPath + "app_webview/Cookies";
+        initView();
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -39,13 +92,43 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+            { WebView.setWebContentsDebuggingEnabled(true); }
+        }
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        mWebView = (WebView) findViewById(R.id.maim_web);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient(this, MainActivity.this));
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(false);
+
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+        mWebView.loadUrl("https://vk.com");
+        verifyStoragePermissions(this);
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            if(mWebView.canGoBack()) {
+                mWebView.goBack();
+            } else {
+                super.onBackPressed();
+            }
         }
     }
 
@@ -72,5 +155,29 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (MyWebChromeClient.mUploadMessagesAboveL != null) {
+            new WebViewHelper(this, MainActivity.this).onActivityResultAboveL(requestCode, resultCode, intent);
+        }
+
+        if (MyWebChromeClient.mUploadMessage == null) return;
+
+        Uri uri = null;
+
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            uri = cameraUri;
+        }
+
+        if (requestCode == REQUEST_CHOOSE && resultCode == RESULT_OK) {
+            uri = afterChosePic(intent);
+        }
+
+        MyWebChromeClient.mUploadMessage.onReceiveValue(uri);
+        MyWebChromeClient.mUploadMessage = null;
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 }
